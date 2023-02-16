7e55da55f6e3b872a94af3e1e203633c73e6e22e
SEUNGHYEON HAN <tmdgus980318@gmail.com>
2023-02-16 15:37:53 +0900

    상품 도메인 개발

--- spring/Personal/jpasjop/src/main/java/jpabook/jpasjop/domain/item/Item.java
+++ spring/Personal/jpasjop/src/main/java/jpabook/jpasjop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpasjop.domain.item;
 
 import jpabook.jpasjop.domain.Category;
+import jpabook.jpasjop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> Categories = new ArrayList<>();
+
+    //== 비즈니스 로직==//
+    // 재고 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ spring/Personal/jpasjop/src/main/java/jpabook/jpasjop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpasjop.repository;
+
+import jpabook.jpasjop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            // 뒤에서 설명함
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
+
