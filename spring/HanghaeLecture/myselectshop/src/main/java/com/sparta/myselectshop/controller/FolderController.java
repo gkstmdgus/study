398885c4ceb162c4ee973e65a15a908d85819423
SEUNGHYEON HAN <tmdgus980318@gmail.com>
2023-02-17 18:56:18 +0900

    Security

--- spring/HanghaeLecture/myselectshop/src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ spring/HanghaeLecture/myselectshop/src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -2,11 +2,14 @@ package com.sparta.myselectshop.controller;
 
 import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -19,21 +22,46 @@ public class FolderController {
     @PostMapping("/folders")
     public List<Folder> addFolders(
             @RequestBody FolderRequestDto folderRequestDto,
-            HttpServletRequest request
+            @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
 
         List<String> folderNames = folderRequestDto.getFolderNames();
 
-        return folderService.addFolders(folderNames, request);
+        System.out.println("======================================================");
+        System.out.println("user.getUsername() = " + userDetails.getUsername());
+        System.out.println("user.getUser() = " + userDetails.getUser());
+        System.out.println("user.getUser().getPassword() = " + userDetails.getUser().getPassword());
+        System.out.println("user.getUser().getId() = " + userDetails.getUser().getId());
+        System.out.println("======================================================");
+
+        return folderService.addFolders(folderNames, userDetails.getUsername());
     }
 
     // 회원이 등록한 모든 폴더 조회
     @GetMapping("/folders")
     public List<Folder> getFolders(
-            HttpServletRequest request
+            @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        return folderService.getFolders(request);
+        return folderService.getFolders(userDetails.getUser());
     }
 
-
+    // 회원이 등록한 폴더 내 모든 상품 조회
+    @GetMapping("/folders/{folderId}/products")
+    public Page<Product> getProductsInFolder(
+            @PathVariable Long folderId,
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam String sortBy,
+            @RequestParam boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return folderService.getProductsInFolder(
+                folderId,
+                page-1,
+                size,
+                sortBy,
+                isAsc,
+                userDetails.getUser()
+        );
+    }
 }
--- spring/HanghaeLecture/myselectshop/src/main/java/com/sparta/myselectshop/service/FolderService.java
+++ spring/HanghaeLecture/myselectshop/src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -1,16 +1,19 @@
 package com.sparta.myselectshop.service;
 
 import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
-import com.sparta.myselectshop.jwt.JwtUtil;
 import com.sparta.myselectshop.repository.FolderRepository;
+import com.sparta.myselectshop.repository.ProductRepository;
 import com.sparta.myselectshop.repository.UserRepository;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,76 +21,62 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FolderService {
 
+    private final ProductRepository productRepository;
     private final FolderRepository folderRepository;
     private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
 
 
     // 로그인한 회원에 폴더들 등록
     @Transactional
-    public List<Folder> addFolders(List<String> folderNames, HttpServletRequest request) {
+    public List<Folder> addFolders(List<String> folderNames, String name) {
 
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+        User user = userRepository.findByUsername(name).orElseThrow(
+                () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+        );
 
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
+        // 입력으로 들어온 폴더 이름을 기준으로, 회원이 이미 생성한 폴더들을 조회합니다.
+        List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
 
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
+        List<Folder> folderList = new ArrayList<>();
 
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-
-            List<Folder> folderList = new ArrayList<>();
-
-            for (String folderName : folderNames) {
+        for (String folderName : folderNames) {
+            // 이미 생성한 폴더가 아닌 경우만 폴더 생성
+            if (!isExistFolderName(folderName, existFolderList)) {
                 Folder folder = new Folder(folderName, user);
                 folderList.add(folder);
             }
-
-            return folderRepository.saveAll(folderList);
-        } else {
-            return null;
         }
+
+        return folderRepository.saveAll(folderList);
     }
 
     // 로그인한 회원이 등록된 모든 폴더 조회
-    public List<Folder> getFolders(HttpServletRequest request) {
+    @Transactional(readOnly = true)
+    public List<Folder> getFolders(User user) {
+        return folderRepository.findAllByUser(user);
+    }
 
-        // 사용자의 정보를 가져온다
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+    @Transactional(readOnly = true)
+    public Page<Product> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
 
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
+        // 페이징 처리
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+
+        return productRepository.findAllByUserIdAndFolderList_Id(user.getId(), folderId, pageable);
+    }
+
+    private boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
+        // 기존 폴더 리스트에서 folder name 이 있는지?
+        for (Folder existFolder : existFolderList) {
+            if (existFolder.getName().equals(folderName)) {
+                return true;
             }
-
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-
-            return folderRepository.findAllByUser(user);
-
-        } else {
-            return null;
         }
+
+        return false;
     }
 
 }
