3af5189add03d63f864e60b08255566c5080357a
SEUNGHYEON HAN <tmdgus980318@gmail.com>
2023-02-18 23:27:21 +0900

    JPA활용 1편 마무리

--- spring/Personal/jpasjop/src/main/java/jpabook/jpasjop/domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpasjop.domain;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- spring/Personal/jpasjop/src/main/java/jpabook/jpasjop/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpasjop.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
